e65d7e3e2ba92902d8b8fef897e9cceabd7f7986
elisha <ebehelisha@gmail.com>
2020-04-23 00:54:05 +0000

    create the plant database

---
+++ app/src/main/java/com/example/sunflower/data/AppDatabase.kt
@@ -0,0 +1,38 @@
+package com.example.sunflower.data
+
+import android.content.Context
+import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.sunflower.utilities.DATABASE_NAME
+import com.example.sunflower.workers.SeedDatabaseWorker
+
+@Database(entities = [GardenPlanting::class, Plant::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun gardenPlanting(): GardenPlantingDao
+    abstract fun plantDao(): PlantDao
+
+    companion object{
+
+        //For singleton instantiation
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase{
+            return instance ?: synchronized(this){
+                instance ?: buildDatabase(context).also { instance = it}
+            }
+        }
+
+        private fun buildDatabase(context: Context): AppDatabase{
+            return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME).addCallback(object: RoomDatabase.Callback(){
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    val request = OneTimeWorkRequestBuilder<SeedDatabaseWorker>().build()
+                    WorkManager.getInstance(context).enqueue(request)
+                }
+            }).build()
+        }
+    }
+}
